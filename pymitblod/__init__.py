9d52174e0ce553e251343b7e79bd26a03624c6ae
Christian <christian@skjerning.eu>
2021-07-02 18:15:04 +0200

    Perhaps fixed building issues

--- pymitblod/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-Init file for pymitblod
-'''
-from .mitblod import MitBlod
-from .consts import Institutions
-
-
-__version__ = '0.0.1'
--- setup.py
+++ setup.py
@@ -1,29 +1,34 @@
 '''
 setup.py
 '''
-from os import path
-from setuptools import setup
-import pymitblod
+
+import setuptools
 
 
-THIS_DIRECTORY = path.abspath(path.dirname(__file__))
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
 
-with open(path.join(THIS_DIRECTORY, 'readme.md'), encoding='utf-8') as f:
-    LONG_DESCRIPTION = f.read()
 
-setup(
+
+setuptools.setup(
     name='pymitblod',
-    version=pymitblod.__version__,
+    version="0.0.1",
+    author='Christian Skjerning',
+    author_email='christian@skjerning.eu',
     description='API wrapper for blood donor webpages.',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
     url='https://github.com/slimcdk/pymitblod',
-    author='Christian Skjerning',
-    author_email='christian@skjerning.eu',
-    license='Apache 2.0',
-    packages=['pymitblod'],
-    install_requires=[
-        'requests==2.25.1',
-        'BeautifulSoup4==4.9.3'
-    ]
+    project_urls={
+        "Bug Tracker": "https://github.com/slimcdk/pymitblod/issues",
+    },
+    license="Apache 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: Apache 2.0",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
